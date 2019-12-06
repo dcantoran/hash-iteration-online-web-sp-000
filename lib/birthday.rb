ef9718398cd137b9aa53cc474bebb998b5bd4bdc
David Cantoran <davidcantoran36@gmail.com>
2019-12-06 01:14:11 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -7,7 +7,7 @@
 def happy_birthday(birthday_kids)
   # add your code snippet here!
   birthday_kids.each do |key, value|
-    if key[value] <= 12
+    if key.value <= 12
       puts "Happy Birthday #{key}! You are now #{value} years old!"
     end 
   end 
